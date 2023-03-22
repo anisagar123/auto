17441c43ebe519ff1ad90814118cf38af7273b7b
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2023-03-22 12:50:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ WELCOM_PIC = environ.get("WELCOM_PIC", "")
 WELCOM_TEXT = environ.get("WELCOM_TEXT", "Hai {user}\nwelcome to {chat}")
 PMFILTER = environ.get('PMFILTER', "True")
 G_FILTER = bool(environ.get("G_FILTER", True))
-BUTTON_LOCK = environ.get("BUTTON_LOCK", "True")
+BUTTON_LOCK = environ.get("BUTTON_LOCK", "False")
 
 # url shortner
 SHORT_URL = environ.get("SHORT_URL")
